e1677a4712a98b9d0357d0052396c779331e3416
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-03-23 03:19:00 +0100

    Update the portfolio.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
 function burger_show()
 {
-    console.log("Showing.");
     document.getElementById("burger-shw").classList.add("hide");
     document.getElementById("burger-cls").classList.remove("hide");
     let l = document.getElementsByClassName("nav-link");
     for (let i = 0; i < l.length; ++i) {
-        l[i].classList.add("show");
+        if (!l[i].classList.contains("hidden")) {
+            l[i].classList.add("show");
+        }
     }
 }
 
 function burger_close()
 {
-    console.log("Hiding.");
     document.getElementById("burger-shw").classList.remove("hide");
     document.getElementById("burger-cls").classList.add("hide");
     let l = document.getElementsByClassName("nav-link");
     for (let i = 0; i < l.length; ++i) {
-        l[i].classList.remove("show");
+        if (!l[i].classList.contains("hidden")) {
+            l[i].classList.remove("show");
+        }
     }
 }
+
+function getUserLanguage()
+{
+    const userLang = navigator.language;
+    if (userLang != "fr" && userLang != "en") {
+        return "en"
+    }
+
+    return userLang;
+}
+
+// Hide elements depending on the language
+const userLang = getUserLanguage();
+document.querySelectorAll("a[lang]").forEach(e => {
+    if (e.lang !== userLang.slice(0, 2)) {
+        e.style.display =  "none";
+    }
+});
+document.querySelectorAll("p[lang]").forEach(e => {
+    if (e.lang !== userLang.slice(0, 2)) {
+        e.style.display =  "none";
+    }
+});
+document.querySelectorAll("a[lang]").forEach(e => {
+    if (e.classList.contains("nav-link") && e.lang !== userLang.slice(0, 2)) {
+        e.classList.add("hidden");
+    }
+});
